dc160bf260ae8f16cfc6c08b6f978ef6ad52cacd
pavvel42 <37178644+pavvel42@users.noreply.github.com>
2026-08-22 09:31:33 +0200

    chore: usunięcie martwego bundle.js i duplikatu manifest.json, ujednolicenie cache-busterów PWA na v10.20

--- static/sw.js
+++ static/sw.js
@@ -1,12 +1,11 @@
 // CineLog Service Worker - Network-First / Stale-While-Revalidate Caching for PWA
-const CACHE_NAME = "cinelog-v10.19.0";
+const CACHE_NAME = "cinelog-v10.20.0";
 const STATIC_ASSETS = [
   "./",
   "manifest.json",
-  "static/css/style.css?v=10.19",
-  "static/js/main.js?v=10.19",
-  "static/js/drive_sync.js?v=10.19",
-  "static/manifest.json",
+  "static/css/style.css?v=10.20",
+  "static/js/main.js?v=10.20",
+  "static/js/drive_sync.js?v=10.20",
   "static/icons/icon-192.png",
   "static/icons/icon-512.png",
   "static/icons/icon-maskable.png",
